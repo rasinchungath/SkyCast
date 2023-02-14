0b931ee25d64ee684def9963992d8f19b2e394c9
rasinchungath <rasincrazz@gmail.com>
2023-02-14 15:46:54 +0530

    initial commit

---
+++ lib/homescreen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCurrentLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+  getCurrentLocation() async {
+    var p = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low,
+      forceAndroidLocationManager: true,
+    );
+    if (p!=null) {
+      print('Latitude:${p.latitude}, Longitude:${p.longitude}');
+      
+    }else{
+      print('Data unavailable');
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:climate_app/homescreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}
+
+
+
